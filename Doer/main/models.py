4f37fe43d4842d85a554afceeb7d429f8df873e1
Hristo Roque <aroquega@unsa.edu.pe>
2020-03-20 14:04:17 -0500

    Addded date attributes to model Task

--- Doer/main/models.py
+++ Doer/main/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Task(models.Model):
     description = models.CharField(max_length=100)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+    start_date = models.DateField(default = date.today())
+    due_date = models.DateField(default = date.today())
+    duration = models.TimeField(blank=True,null=True)
